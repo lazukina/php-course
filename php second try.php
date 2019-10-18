cb7f4967d673df1bc9263474a5af7bbd5222fbeb
Lazukina <lazukina91@gmail.com>
2019-10-18 17:35:01 +0300

    Pie is lie

---
+++ php second try.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <?php $browser = get_browser(null, true); ?>
+  <h1>Информация о пользователе</h1>
+  <dl>
+    <dt><strong>Ваш IP адрес:</strong></dt>
+    <dd><en><?php print($_SERVER['REMOTE_ADDR']); ?></en></dd>
+
+    <dt><strong></strong></dt>
+
+  </dl>
+</body>
+</html>
