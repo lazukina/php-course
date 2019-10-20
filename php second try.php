ebcd1b9f5a3ac9eff07ed75105d2a2be18c9c783
Lazukina <lazukina91@gmail.com>
2019-10-20 13:20:05 +0300

    idk

--- php second try.php
+++ php second try.php
@@ -12,7 +12,7 @@
   <dl>
     <dt><strong>Ваш IP адрес:</strong></dt>
     <dd><en><?php print($_SERVER['REMOTE_ADDR']); ?></en></dd>
-<!-- still testing this bullshit of a broken system  -->
+<!-- wonderfull-->
     <dt><strong></strong></dt>
 
   </dl>
